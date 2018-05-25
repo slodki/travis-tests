43ed3ed2342cb71176ac1fe81a761b567560e069
Tomasz Słodkowicz <slodki@users.noreply.github.com>
2018-05-25 16:31:11 +0200

    qqaaa

--- list_github.sh
+++ list_github.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo 'Filenames at GitHub Releases:'
-curl -q -H "Authorization: token $GitHub_auth_token" \
+curl -s -H "Authorization: token $GitHub_auth_token" \
     https://api.github.com/repos/$TRAVIS_REPO_SLUG/releases/tags/$TRAVIS_TAG \
   | grep \"name\"
 true
